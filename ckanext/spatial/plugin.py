96ed3d7ba0c1a32e42c7ee20cecd69e029fbac26
Ian Ward <ian@excess.org>
2013-03-06 19:24:58 -0500

    make spatial field optional

--- ckanext/spatial/plugin.py
+++ ckanext/spatial/plugin.py
@@ -71,7 +71,7 @@ class SpatialMetadata(SingletonPlugin):
         # TODO: deleted extra
         for extra in package.extras_list:
             if extra.key == 'spatial':
-                if extra.state == 'active':
+                if extra.state == 'active' and extra.value:
                     try:
                         log.debug('Received: %r' % extra.value)
                         geometry = json.loads(extra.value)
@@ -94,7 +94,7 @@ class SpatialMetadata(SingletonPlugin):
                         error_dict = {'spatial': [u'Error: %s' % str(e)]}
                         raise ValidationError(error_dict, error_summary=package_error_summary(error_dict))
 
-                elif extra.state == 'deleted':
+                elif (extra.state == 'active' and not extra.value) or extra.state == 'deleted':
                     # Delete extent from table
                     save_package_extent(package.id, None)
 
